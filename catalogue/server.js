4617e87c2f99a0f14458ca9c2373f6d8d9bedd07
siva <siva@ivs.tv>
2023-03-24 20:54:43 +0530

    msg

--- catalogue/server.js
+++ catalogue/server.js
@@ -154,7 +154,7 @@ app.get('/search/:text', (req, res) => {
 // set up Mongo
 function mongoConnect() {
     return new Promise((resolve, reject) => {
-        var mongoURL = process.env.MONGO_URL || 'mongodb://mongodb:27017/catalogue';
+        var mongoURL = process.env.MONGO_URL || 'mongodb://mongodb-service:27017/catalogue';
         mongoClient.connect(mongoURL, (error, client) => {
             if(error) {
                 reject(error);
